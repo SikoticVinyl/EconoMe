e8cd5886ee534122209b512ef76807825602b32e
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-01 22:56:43 -0700

    created user model for DB.

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: [true, 'Please provide your full name']
+  },
+  username: {
+    type: String,
+    required: [true, 'Please provide a username'],
+    unique: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    select: false // Prevents password from being returned in queries by default
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
