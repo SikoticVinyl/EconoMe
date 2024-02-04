8aa3a895e94f03742aadd560fc3c1f959dddca60
jjthedev <jjthedev94@gmail.com>
2024-02-04 15:35:56 -0700

    added components for front end

---
+++ client/src/Components/Signup.jsx
@@ -0,0 +1,20 @@
+function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
