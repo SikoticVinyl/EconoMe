855a4a2ba5b58aacdf81d47eb50f2b695c9fbcb6
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-04 01:04:34 -0700

    budget model built out.

---
+++ server/models/Budget.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { schema: categorySchema } = require('./Category');
+
+const budgetSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  categories: [categorySchema], 
+});
+
+const Budget = mongoose.model('Budget', budgetSchema);
+module.exports = Budget;
