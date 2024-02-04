d2c4bb5560a5d69c2dda4dbdf5d6c097b50c1c5c
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-03 19:56:47 -0700

    import and setup routes on app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
 
 console.log("Trying to connect to: ", process.env.MONGODB_URI);
+
+const app = express();
+app.use(express.json()); // Middleware for parsing JSON bodies
+
+app.use('/api/users', authRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
