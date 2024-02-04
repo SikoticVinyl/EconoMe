6a595ff02d3a7931d6d363f63d3166965b4ffb52
jjthedev <jjthedev94@gmail.com>
2024-02-04 15:35:43 -0700

    Added login and sign up components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Login from './Login';
+import Signup from './Signup';
 import './App.css'
 
 function App() {
@@ -9,14 +11,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
       </div>
       <h1>Vite + React</h1>
+      <Login />
+      <Signup />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
