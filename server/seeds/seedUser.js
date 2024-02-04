7336b877e497719ccd79ed5ad036884922a07aa7
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-03 20:59:32 -0700

    added delete to seed for dev

--- server/seeds/seedUser.js
+++ server/seeds/seedUser.js
@@ -2,9 +2,17 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
-mongoose.connect('mongodb://127.0.0.1:27017/econome')
+console.log("Log of .env being pulled: ", process.env.MONGODB_URI)
+
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Delete prior seeded data
+//!!DEVELOPMENT ONLY!!
+User.deleteMany({})
+  .then(() => {
+    console.log('Deleted all users');
 
 // Development / example user
 const seedUser = new User({
