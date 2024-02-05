23c39371eaf6085a4f6b960283500093fc9e3bc8
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-04 19:07:02 -0700

    basic transaction schema started.

---
+++ server/graphql/schemas/transactionSchema.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express');
+
+const transactionSchema = gql`
+  type Transaction {
+    id: ID!
+    name: String!
+    amount: Float!
+    transactionType: String!
+    dueDate: String
+    payDate: String
+    flexible: Boolean
+    paid: Boolean
+    category: Category
+  }
+
+  extend type Query {
+    transaction(id: ID!): Transaction
+    transactions: [Transaction]
+  }
+
+  extend type Mutation {
+    createTransaction(name: String!, amount: Float!, transactionType: String!, dueDate: String, payDate: String, flexible: Boolean, paid: Boolean, categoryId: ID!): Transaction
+  }
+`;
+
+module.exports = transactionSchema;
