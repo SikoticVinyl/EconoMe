842b7d2bf54d922ddaf16e372eb8e415b837c59d
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-04 21:18:11 -0700

    user login via graphql and signup fully working. Other schemas built out for all models.

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const helmet = require('helmet');
@@ -17,9 +17,11 @@ async function startServer() {
   }));
   app.use(express.json());
 
-  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
+  console.log("app.js connect: ", process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
   const server = new ApolloServer({
     schema, 
--- server/graphql/schemas/index.js
+++ server/graphql/schemas/index.js
@@ -1,6 +1,16 @@
 //Tools for schema creation and merging
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge');
+const { gql } = require('apollo-server-express');
+
+const baseSchema = gql`
+  type Query {
+    _empty: String
+  }
+  type Mutation {
+    _empty: String
+  }
+`;
 
 // Type definitions
 const userSchema = require('./userSchema');
@@ -9,7 +19,7 @@ const categorySchema = require('./categorySchema');
 const budgetSchema = require('./budgetSchema');
 
 // Merge the type definitions
-const typeDefs = mergeTypeDefs([userSchema, transactionSchema, categorySchema, budgetSchema]);
+const typeDefs = mergeTypeDefs([baseSchema, userSchema, transactionSchema, categorySchema, budgetSchema]);
 
 //Corresponding resolvers
 const userResolvers = require('../resolvers/userResolvers');
