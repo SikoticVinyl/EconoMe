cba8c433858f65f3b26ffc162596983afdda588b
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-04 19:14:12 -0700

    added filter to category db model

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,17 +1,23 @@
+const mongoose = require('mongoose');
 const { schema: transactionSchema } = require('./Transaction');
 
 const categorySchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, 'Please provide a name for the category']
-    },
-    flexB: {
-      type: Boolean,
-      required: [true, 'Please specify if the category is for flexible budgeting']
-    },
-    transactions: [transactionSchema]
-  });
-  
+  name: {
+    type: String,
+    required: [true, 'Please provide a name for the category'],
+  },
+  flexB: {
+    type: Boolean,
+    required: [true, 'Please specify if the category is for flexible budgeting'],
+  },
+  transactions: [transactionSchema],
+  filter: {
+    type: String,
+    enum: ['Income', 'Expenses', 'Savings'],
+    required: [true, 'Please specify the category filter'],
+  },
+});
+
 const Category = mongoose.model('Category', categorySchema);
 
 module.exports.Category = Category;
