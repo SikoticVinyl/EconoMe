36fd73e44be1adf67e16ea7b6c57b0a1b1dd1244
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-04 17:09:54 -0700

    export verify token function for JWT to get and verify jwt from frontend.

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
+module.exports = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1]; // Assuming 'Bearer <token>'
 
   if (!token) {
