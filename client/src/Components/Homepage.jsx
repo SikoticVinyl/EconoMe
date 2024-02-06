2cdc2d684fa0a709d3212bbecb7bdbbd5db1dc9b
jjthedev <jjthedev94@gmail.com>
2024-02-05 18:46:36 -0700

    Deleted code

--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <div className='"container mx-auto px-4 sm:px-6 lg:px-8"'>
-      <h1>Econ-o-Me</h1>
-      <p>Got 5 on it? </p>
-      <p>Lets see</p>
+      <h1>Econ-o-Me</h1>  {/* maybe add image instead */}
+    
+      <p>Got $5 on it?</p>
+      
       <Link to="/login">Login</Link>
       <br />
       <Link to="/signup">Signup</Link>
