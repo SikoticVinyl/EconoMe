4dfc6864c69b2e12bfe0d89690d7d9193b07f199
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-07 19:43:12 -0700

    began build out of service budget services.

---
+++ server/services/budgetService.js
@@ -0,0 +1,6 @@
+const Transaction = require('../models/Transaction');
+const mongoose = require('mongoose');
+
+exports.getTotalIncome = async (userId) => {
+    // MongoDB aggregation query here
+  };
