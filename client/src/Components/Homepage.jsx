e63d015e102a175e2441921e6f346e21f5b0ab34
jjthedev <jjthedev94@gmail.com>
2024-02-08 21:13:53 -0700

    design the pages

--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div className='"container mx-auto px-4 sm:px-6 lg:px-8"'>
-      <h1>Econ-o-Me</h1>  {/* maybe add image instead */}
-    
-      <p>Got $5 on it?</p>
+    <div className="bg-gradient-to-b from-blue-500 to-green-500 min-h-screen">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <h1>Econ-o-Me</h1>  {/* maybe add image instead */}
       
-      <Link to="/login">Login</Link>
-      <br />
-      <Link to="/signup">Signup</Link>
+        <p>Got $5 on it?</p>
+        
+        <Link to="/login">Login</Link>
+        <br />
+        <Link to="/signup">Signup</Link>
+      </div>
     </div>
   );
 }
