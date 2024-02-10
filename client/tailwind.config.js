33f0284af91e2d581b8a4ce63b32bf1a6a53d2b1
jjthedev <jjthedev94@gmail.com>
2024-02-10 13:24:54 -0700

    Added images to use and Configured fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,6 +15,7 @@ export default {
         'Sixtyfour':  ['Sixtyfour', 'sans-serif'],
         'rubik-doodle': ['Rubik Doodle Shadow', 'system-ui'],
         'moirai': ['"Moirai One"', 'system-ui'],
+        'old-standard-tt': ['Old Standard TT', 'serif'],
       },
       colors: {
         'color1': '#d7d4db', // light grey
@@ -22,12 +23,22 @@ export default {
         'color3': '#184461', // dark blue
         'color4': '#1c5d3b', // green
         'color5': '#044c26', // dark green
+        'persimmon': '#FF5733',  // Persimmon
+        'red-pigment': '#C70039',  // Red Pigment
+        'tyrian-purple': '#900C3F',  // Tyrian Purple
+        'pansy-purple': '#581845',  // Pansy Purple
+        'oxford-blue': '#154360',  // Oxford Blue
+        'prussian-blue': '#1A5276',  // Prussian Blue
+        'cool-black': '#1F618D',  // Cool Black
+        'air-force-blue': '#2471A3',  // Air Force Blue
+        'cerulean': '#2980B9',  // Cerulean
+        'vivid-cerulean': '#2E86C1',  // Vivid Cerulean
       },
       backgroundImage: {
         'gradient-to-t': 'linear-gradient(to top, var(--tw-gradient-stops))',
         'blend-luminosity': 'luminosity',
         'blend-mode': 'blend-mode',
-        'old-standard-tt': ['Old Standard TT', 'serif'],
+        'custom-background': "url('/Background2Hp.jpg')",
      },
     },
   },
