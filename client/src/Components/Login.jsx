87c4c0f3f70be575494bf389349afd2907625527
Trevor Christensen <trevorchristensen3405@gmail.com>
2024-02-12 22:05:28 -0700

    fixed login.jsx import

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../client-graphql/queries/login';
+import { LOGIN_USER } from '../client-graphql/queries/loginUser.jsx';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
