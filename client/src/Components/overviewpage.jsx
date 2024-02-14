58f6c43a0448d406b5182e993de6b73805b96bfb
jjthedev <jjthedev94@gmail.com>
2024-02-13 17:43:35 -0700

    design overview page

--- client/src/Components/overviewpage.jsx
+++ client/src/Components/overviewpage.jsx
@@ -14,12 +14,12 @@ function OverviewPage() {
     <div
       className="flex flex-col h-screen justify-center items-center relative"
       style={{
-        backgroundImage: `url(/Overviewpage1.jpg)`,
+        backgroundImage: `url(/Moneybg.jpg)`,
         backgroundSize: "cover",
       }}
     >
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 className="mb-6 text-xl font-bold">Overview</h1>
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" style={{ backgroundImage: `url(/Wood.jpg)`, backgroundSize: "cover" }}>
+        <h1 className="mb-6 text-5xl font-bold font-rubik-doodle text-center">Overview</h1>
         <div className="mb-6 flex justify-between items-center space-x-4">
           <label className="text-gray-700 text-sm font-bold mb-2 w-1/3">Income Total:</label>
           <input type="number" value={incomeTotal} onChange={handleIncomeChange} placeholder="Income Total" className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
@@ -32,7 +32,7 @@ function OverviewPage() {
           <label className="text-gray-700 text-sm font-bold mb-2 w-1/3">Savings Goals:</label>
           <input type="number" value={savingsGoals} onChange={handleSavingsGoalsChange} placeholder="Savings Goals" className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
         </div>
-        <p className="mb-6 text-gray-700 text-base">Profit: {profit}</p>
+        <p className="mb-6 text-gray-700 text-2xl font-bold bg-green-200 text-green-700 p-3 rounded-md shadow-lg rubik-doodle text-center">Profit $: {profit}</p>
       </div>
       <Link to="/update-income" className="mb-6 inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">Update Income</Link>
       <div className="mb-20">
